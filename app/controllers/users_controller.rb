f669fdabd90560cccbc9f37baa2bc88c76e50c7c
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2022-10-12 01:07:06 +0900

    users_controlに役職追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     @name = user.name
     @profile = user.profile
     @occupation = user.occupation
+    @occupation = user.position
     @prototypes = user.prototypes
   end
 end
